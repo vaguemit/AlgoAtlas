8740fd77305e122c1cb3e3c2fc74600bfce7b8b3
vaguemit <mitparikh1046@gmail.com>
2025-04-09 16:34:22 +0530

    google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { cn } from "@/lib/utils"
 import { AssistantWrapper } from "@/components/assistant-wrapper"
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -55,6 +56,15 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark scroll-smooth">
       <head>
         <link rel="icon" href="/compass-logo.png" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WSDLPCPT');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={cn(
@@ -62,6 +72,17 @@ export default function RootLayout({
           inter.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSDLPCPT"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
